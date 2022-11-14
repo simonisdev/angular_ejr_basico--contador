59924b9754c19e713ef1930a2ccfa614321ee1de
simonisdev <sagredo83@gmail.com>
2022-11-14 17:05:40 -0300

    FormsModule - ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { ContadorModule } from "./contador/contador.module";
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
